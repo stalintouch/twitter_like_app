014fa166edb86ce33c2923526127ea53fc816ea6
stalintouch <stalintouch1983@gmail.com>
2016-08-13 19:00:36 -0400

    Adding micropost to users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
@@ -12,3 +13,4 @@ class StaticPagesController < ApplicationController
   end
   
 end
+
