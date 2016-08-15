ad89bde7843921cef4ae55e603aa8cef6f7e652b
stalintouch <stalintouch1983@gmail.com>
2016-08-15 18:53:10 -0400

    Add images to micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,5 +3,14 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   default_scope -> { order(created_at: :desc) }
-end
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
 
+  private
+
+    def picture_size
+      if picture.size > 5.megabytes
+        erros.add(:picture, "should be less than 5MB")
+      end
+    end
+end
