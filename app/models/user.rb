7d597b7c17b2e804ba5b7f3d00027cdd0b7ebf0d
stalintouch <stalintouch1983@gmail.com>
2016-08-23 19:16:25 -0400

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,15 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:  :destroy
+  has_many :followers, through: :passive_relationships, source: :follower                             
+  has_many :following, through: :active_relationships, source: :followed                               
+
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -69,6 +78,29 @@ class User < ApplicationRecord
   def feed
     Micropost.where("user_id = ?", id)
   end
+
+  #Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  #Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+   # Returns a user's status feed.
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                        WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                      OR user_id = :user_id", user_id: id)                    
+  end
   
   private
 
@@ -83,4 +115,15 @@ class User < ApplicationRecord
       self.activation_digest = User.digest(activation_token)
     end
 end
+
+
+
+
+
+
+
+
+
+
+
 
